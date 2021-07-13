2c22ad01e9e987ce64df576d059e8f287fe77318
jcdude1998 <67282795+jcdude1998@users.noreply.github.com>
2021-07-13 18:09:41 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using Twilio;
+using Twilio.Rest.Lookups.V1;
+
+namespace TwillioBulkLookup
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Twilio Bulk LookUp!");
+
+            TwilioClient.Init("", "");
+
+            var type = new List<string> {
+            "carrier","caller-name"
+            }   ;
+
+            Console.WriteLine("Enter Phone Number");
+            string phoneNumber = Console.ReadLine();
+
+            var phoneNumberLookUp = PhoneNumberResource.Fetch(
+                type: type,
+                pathPhoneNumber: new Twilio.Types.PhoneNumber("+1"+phoneNumber)
+            );
+
+            Console.WriteLine("Response!");
+            Console.WriteLine(phoneNumberLookUp.Carrier);
+            Console.WriteLine(phoneNumberLookUp.CallerName);
+        }
+    }
+}
