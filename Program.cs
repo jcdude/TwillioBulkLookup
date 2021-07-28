638bb8af15980f37431e5c991bf94fa5db54c4a6
jcdude1998 <67282795+jcdude1998@users.noreply.github.com>
2021-07-28 15:48:32 -0400

    Added options to the command line

--- Program.cs
+++ Program.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 using System.Data;
 using System.IO;
 using System.Reflection;
+using System.Text;
 using Twilio;
 using Twilio.Rest.Lookups.V1;
 using TwillioBulkLookup.Models;
@@ -21,11 +22,13 @@ namespace TwillioBulkLookup
             string appSettings = Path.Combine(appPath, "appsettings.json");
             string inputPath = Path.Combine(appPath, "Input");
             string outputPath = Path.Combine(appPath, "Output","outPut"+DateTime.Now.ToString("yyyyMMddhhmmss") + ".xlsx");
+            string outputPathJson = Path.Combine(appPath, "Output", "outPut" + DateTime.Now.ToString("yyyyMMddhhmmss") + ".json");
 
             List<string> phoneNumbers = new List<string>();
             XLWorkbook wb = new XLWorkbook();
             AppSettings appSettingsDto = new AppSettings();
             DataTable phoneDetails = new DataTable();
+            StringBuilder sb = new StringBuilder();
 
             using (StreamReader r = new StreamReader(appSettings))
             {
@@ -68,20 +71,54 @@ namespace TwillioBulkLookup
              "ekata_reverse_phone"//,"ekata_phone_valid"
             };
 
-            Console.WriteLine("Type 1 and press enter to include addons else just hit enter");
-            if (Console.ReadLine() == "")
+            Console.WriteLine("Type 1/2/3/4 and press enter to include addons and hit enter");
+            var addOnSelection = Console.ReadLine();
+            if (addOnSelection == "1")
             {
                 addOns = new List<string>();
+            }
+            else if(addOnSelection == "2")
+            {
+                addOns = new List<string> {
+                 "ekata_reverse_phone"
+                };
+            }
+            else if (addOnSelection == "3")
+            {
+                addOns = new List<string> {
+                 "ekata_phone_valid"
+                };
+            }
+            else if (addOnSelection == "4")
+            {
+                addOns = new List<string> {
+                 "ekata_reverse_phone","ekata_phone_valid"
+                };
             }
             phoneDetails.Columns.Add("phone_number");
             phoneDetails.Columns.Add("carrier_name");
             phoneDetails.Columns.Add("carrier_type");
+            phoneDetails.Columns.Add("mobile_country_code");
+            phoneDetails.Columns.Add("mobile_network_code");
             phoneDetails.Columns.Add("caller_name");
 
-            if (addOns.Count > 0)
+            if (addOnSelection == "2" || addOnSelection == "4")
             {
                 phoneDetails.Columns.Add("caller_fullname");
-                phoneDetails.Columns.Add("caller_address");
+                phoneDetails.Columns.Add("is_commercial");
+                phoneDetails.Columns.Add("gender");
+                phoneDetails.Columns.Add("line_type");
+            }
+            else if (addOnSelection == "3" || addOnSelection == "4")
+            {
+                
+            }
+            else if (addOnSelection == "4")
+            {
+                phoneDetails.Columns.Add("caller_fullname");
+                phoneDetails.Columns.Add("is_commercial");
+                phoneDetails.Columns.Add("gender");
+                phoneDetails.Columns.Add("line_type");
             }
 
             foreach (string phoneNumber in phoneNumbers)
@@ -98,17 +135,13 @@ namespace TwillioBulkLookup
                 );
 
                 Console.Write("=");
+                                
+                sb.Append(phoneNumberLookUp.Carrier.ToString());
+                sb.Append(phoneNumberLookUp.CallerName.ToString());
 
                 Carrier carrier = JsonConvert.DeserializeObject<Carrier>(phoneNumberLookUp.Carrier.ToString());
 
                 CallerName callerName = JsonConvert.DeserializeObject<CallerName>(phoneNumberLookUp.CallerName.ToString());
-
-                DataRow dr = phoneDetails.NewRow();
-
-                dr["phone_number"] = phoneNumber;
-                dr["carrier_name"] = carrier.name;
-                dr["carrier_type"] = carrier.type;
-                dr["caller_name"] = callerName.caller_name;
 
                 if (phoneNumberLookUp.AddOns != null)
                 {
@@ -116,17 +149,82 @@ namespace TwillioBulkLookup
 
                     if (addOn.results.ekata_reverse_phone.status == "successful")
                     {
+                        if (addOn.results.ekata_reverse_phone.result.current_addresses.Length > 0)
+                        {
+                            int addressCount = 0;
+                            foreach (var address in addOn.results.ekata_reverse_phone.result.current_addresses)
+                            {
+                                phoneDetails.Columns.Add("caller_address_"+addressCount);
+                                addressCount++;
+                            }
+
+                        }
+
+                        if (addOn.results.ekata_reverse_phone.result.alternate_phones.Length > 0)
+                        {
+                            int phoneCount = 0;
+                            foreach (var alternatePhone in addOn.results.ekata_reverse_phone.result.alternate_phones)
+                            {
+                                phoneDetails.Columns.Add("alternate_phones_" + phoneCount);
+                                phoneCount++;
+                            }
+
+                        }
+                    }
+                }
+
+                DataRow dr = phoneDetails.NewRow();
+
+                dr["phone_number"] = phoneNumber;
+                dr["carrier_name"] = carrier.name;
+                dr["carrier_type"] = carrier.type;
+                dr["mobile_country_code"] = carrier.mobile_country_code ?? "";
+                dr["mobile_network_code"] = carrier.mobile_network_code ?? "";
+                dr["caller_name"] = callerName.caller_name;
+
+                if (phoneNumberLookUp.AddOns != null)
+                {
+                    AddOn addOn = JsonConvert.DeserializeObject<AddOn>(phoneNumberLookUp.AddOns.ToString());
+
+                    sb.Append(phoneNumberLookUp.AddOns.ToString());
+
+                    if (addOn.results.ekata_reverse_phone.status == "successful")
+                    {
                         dr["caller_fullname"] = addOn.results.ekata_reverse_phone.result.belongs_to.name ?? "";
+                        dr["is_commercial"] = addOn.results.ekata_reverse_phone.result.is_commercial.ToString();
+                        dr["gender"] = addOn.results.ekata_reverse_phone.result.belongs_to.gender ?? "";
+                        dr["line_type"] = addOn.results.ekata_reverse_phone.result.line_type ?? "";
 
                         if (addOn.results.ekata_reverse_phone.result.current_addresses.Length > 0)
                         {
-                            var address = addOn.results.ekata_reverse_phone.result.current_addresses[0];
-                            dr["caller_address"] = address.street_line_1 ?? "" +
-                                address.street_line_2 ?? "" + " " +
-                                address.city ?? "" + " " +
-                                address.state_code ?? "" + " " +
-                                address.zip4 ?? "";
+                            int addressCount = 0;
+                            foreach (var address in addOn.results.ekata_reverse_phone.result.current_addresses)
+                            {
+                                dr["caller_address_"+addressCount] = (address.street_line_1 ?? "") +
+                                    (address.street_line_2 ?? "") + " " +
+                                    (address.city ?? "") + " " +
+                                    (address.state_code ?? "") + " " +
+                                    (address.zip4 ?? "");
+                                addressCount++;
+                            }
+                            
                         }
+
+                        if (addOn.results.ekata_reverse_phone.result.alternate_phones.Length > 0)
+                        {
+                            int phoneCount = 0;
+                            foreach (var alternatePhone in addOn.results.ekata_reverse_phone.result.alternate_phones)
+                            {
+                                dr["alternate_phone_" + phoneCount] = (alternatePhone.line_type ?? "") + ":" + (alternatePhone.phone_number ?? "");
+                                phoneCount++;
+                            }
+
+                        }
+                    }
+
+                    if(addOn.results.ekata_phone_valid.status == "successful")
+                    {
+                        //addOn.results.ekata_phone_valid.result.
                     }
                 }
 
@@ -136,6 +234,11 @@ namespace TwillioBulkLookup
             wb.Worksheets.Add(phoneDetails, "LookUp Results");
 
             wb.SaveAs(outputPath);
+
+            using (System.IO.StreamWriter file = new System.IO.StreamWriter(outputPathJson))
+            {
+                file.WriteLine(sb.ToString()); // "sb" is the StringBuilder
+            }
 
             Console.WriteLine("Complete");
             Console.ReadLine();
